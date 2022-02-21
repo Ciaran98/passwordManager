814c5024c27caabc85ef485a1ce68cbf2a00b441
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-02-21 14:23:58 +0000

    Updates to the main file

--- pm.py
+++ pm.py
@@ -13,11 +13,11 @@ def create_file(filename):
 
 
 
-# This method is used to create the desired format for the json file
+# This method is used to create the desired format for the storage json file
 def import_format(filename):
 	json_file_format = """{
-    "passworddata": [
-    ]
+	"passworddata": [
+	]
 }
 """
 	to_python = json.loads(json_file_format)
@@ -26,20 +26,6 @@ def import_format(filename):
 
 
 
-# This next portion determines where the users documents folder is located
-docPath = R"C:\Users\$USERNAME\Documents"
-docPath = os.path.expandvars(docPath) + "\PMPro"
-
-
-# A try except block is used to make sure the file does not already exist, and if it does, it will back out and throw the exception
-if not os.path.exists(docPath):
-	os.mkdir(docPath)
-
-
-
-# Append the name of the json file we wish to use for storage to the end of the filepath
-confPath = docPath + "\config.json"
-docPath = docPath+ "\PMStorage.json"
 
 
 # generate new cipher key, will only be used if there is no config file / cipher key already
@@ -52,6 +38,7 @@ def gen_key_file(filename):
 		json.dump(conf, confWrite)
 
 
+
 # Read the cipherkey from the config file
 def read_ciph_key(filename):
 	with open(filename, 'r') as confRead:
@@ -59,19 +46,39 @@ def read_ciph_key(filename):
 		ciph = bytes(data['key'], 'UTF-8')
 	return ciph
 
-# Check if the file path does not already exist, if it doesn't, the program will execute the create_file method, creating the JSON file in the desired path
-# Does nothing if the file already exists. These two if statements create both the json storage file, and the config file
+def get_filepath():
+	filepath = R"C:\Users\$USERNAME\Documents\PMPro\PMStorage.json"
+	filepath = os.path.expandvars(filepath)
+	return filepath
+
+	# This next portion determines where the users documents folder is located
+docPath = R"C:\Users\$USERNAME\Documents"
+docPath = os.path.expandvars(docPath) + "\PMPro"
+
+	# Check if the folder already exists, if it doesn't, make a new one
+if not os.path.exists(docPath):
+	os.mkdir(docPath)
+
+	# Append the name of the json file we wish to use for storage to the end of the filepath
+confPath = docPath + "\config.json"
+docPath = docPath+ "\PMStorage.json"
+
+
+	# Check if the file path does not already exist, if it doesn't, the program will execute the create_file method, creating the JSON file in the desired path
+	# Does nothing if the file already exists. These two if statements create both the json storage file, and the config file
 if not os.path.exists(docPath):
 	create_file(docPath)
 	import_format(docPath)
-
 
 if not os.path.exists(confPath):
 	create_file(confPath)
 	gen_key_file(confPath)
 
+
 # Initiates the cipherKey Variable that will be used in the encryption of your passwords
 cipherKey = read_ciph_key(confPath)
+
+
 
 # Read the data from the json file
 def get_data(filename):
@@ -79,7 +86,6 @@ def get_data(filename):
 		data = json.load(f)
 		for x in range(len(data["passworddata"])):
 			print("Platform: " + data["passworddata"][x]["platform"] + " Password: " + data["passworddata"][x]["encrypted"])
-	f.close()
 
 
 
@@ -88,7 +94,6 @@ def get_password_from_index(filename, index):
 	with open(filename, 'r') as f:
 		data = json.load(f)
 		encryptedPass = bytes(data["passworddata"][index]["encrypted"],'UTF-8')
-	f.close()
 	return encryptedPass
 
 
@@ -100,7 +105,6 @@ def get_password_from_platform_name(filename, platform):
 		for x in range(len(data["passworddata"])):
 			if platform == data["passworddata"][x]["platform"]:
 				print("Not sure if this will be used")
-	f.close()
 
 
 
@@ -111,7 +115,6 @@ def write_data(new_data,filename):
 		file_data["passworddata"].append(new_data)
 		f.seek(0)
 		json.dump(file_data, f, indent = 4)
-		f.close()
 
 
 
@@ -143,28 +146,29 @@ def encrypt_password(password):
 def decrypt_password(encryptedPass):
 	cipher = Fernet(cipherKey)
 	decryptedPass = cipher.decrypt(encryptedPass)
-	print(decryptedPass.decode("utf-8"))
+	return decryptedPass.decode("utf-8")
 
 
 # Get number of passwords stored in the json file
 def get_password_count(filename):
 	with open(filename, 'r') as f:
 		data = json.load(f)
-		print(len(data["passworddata"]))
+		return len(data["passworddata"])
 
 
 # Match case statement for carrying out user designated operations
 def perform_operation(operation):
-    match operation:
-        case 'list':
-        	get_data(docPath)
-        case 'write':
-        	email = input("Please enter your Email Address: ")
-        	password = input("Please enter your Password: ")
-        	platform = input("Please enter the platform associated with this account: ")
-        	data = prepare_input(email,platform,password)
-        	write_data(data,docPath)
-        case 'get':
-        	index = int(input("Which password do you want to retrieve: "))
-        	encryptedPassword = get_password_from_index(docPath, index)
-        	decrypt_password(encryptedPassword)
+	match operation:
+		case "list":
+			get_data(docPath)
+		case "write":
+			email = input("Please enter your Email Address: ")
+			password = input("Please enter your Password: ")
+			platform = input("Please enter the platform associated with this account: ")
+			data = prepare_input(email,platform,password)
+			write_data(data,docPath)
+		case 'get':
+			index = int(input("Which password do you want to retrieve: "))
+			encryptedPassword = get_password_from_index(docPath, index)
+			password = decrypt_password(encryptedPassword)
+			print(password)
